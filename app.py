988fe447a2a74b649d4479bdca9ca5f56757bd2a
Amit Sukhadiya <amit-gaurangbhai.sukhadiya@siemens.com>
2023-09-11 17:53:33 +0200

    change the port of app

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def home():
     return render_template('index.html', template_folder=template_folder)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8083))
     app.run(debug=True, host='0.0.0.0', port=port)
